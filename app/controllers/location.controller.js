5bf98e10065628ee6ecd593247c8add4c2a040b5
Salvatore Dessupoiu <salvatore@rameplatform.com>
2025-03-01 13:53:23 +0100

    Refactor controllers to use async/await mongoose structure

--- app/controllers/location.controller.js
+++ app/controllers/location.controller.js
@@ -1,43 +1,38 @@
-var Location = require('../models/location.model.js');
+const Location = require('../models/location.model.js');
 
-exports.create = function(req, res) {
+exports.create = async function(req, res) {
     // Create and Save a new Location
     if(!req.body.points) {
         res.status(400).send({message: "location can not be empty"});
     }
 console.log(req.body);
     var location = new Location({location_name: req.body.location_name, points: req.body.points, emotion: req.body.emotion});
-
-    location.save(function(err, data) {
-        if(err) {
-            console.log(err);
-            res.status(500).send({message: "Some error occurred while creating the Location."});
-        } else {
-            res.send(data);
-        }
-    });
+    try {
+        res.send(await Location.create(location));   
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({message: "Some error occurred while creating the Location.", error: error});
+    }
 };
 
-exports.findAll = function(req, res) {
+exports.findAll = async function(req, res) {
     // Retrieve and return all notes from the database.
-    Location.find(function(err, notes){
-        if(err) {
-            res.status(500).send({message: "Some error occurred while retrieving locations."});
-        } else {
-            res.send(notes);
-        }
-    });
+    try {
+        res.send(await Location.find().exec());
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({message: "Some error occurred while retrieving notes.", error: error});
+    }
 };
 
-exports.findOne = function(req, res) {
+exports.findOne = async function(req, res) {
     // Find a single note with a noteId
-    Location.findById(req.params.noteId, function(err, data) {
-        if(err) {
-            res.status(500).send({message: "Could not retrieve location with id " + req.params.noteId});
-        } else {
-            res.send(data);
-        }
-    });
+    try {
+        res.send(await Location.findById(req.params.noteId).exec());
+    }
+    catch (error) {
+        res.status(500).send({message: "Some error occurred while retrieving notes.", error: error});
+    }
 };
 
 exports.findLocation = function(req, res, next) {
@@ -53,57 +48,45 @@ exports.findLocation = function(req, res, next) {
       // get coordinates [ <longitude> , <latitude> ]
       var coords = [];
       coords[0] = parseFloat(req.query.longitude) || 0;
-  coords[1] = parseFloat(req.query.latitude) || 0;
+      coords[1] = parseFloat(req.query.latitude) || 0;
 
       // find a location
-      Location.aggregate(
-        [
-{
-      $geoNear: {
-       near: { type: "Point", coordinates: coords },
-       distanceField: "dist.calculated",
-       includeLocs: "dist.location",
-       spherical: true
-    }
-}
-  ]
-      ).limit(limit).exec(function(err, locations) {
-        if (err) {
-        console.log(err);
-        res.status(500).send({message: "Some error occurred while retrieving locations."});
-
-        }
-        res.send(locations);
-      });
-};
-
-exports.update = function(req, res) {
-    // Update a note identified by the noteId in the request
-    Location.findById(req.params.noteId, function(err, note) {
-        if(err) {
-            res.status(500).send({message: "Could not find a note with id " + req.params.noteId});
-        }
-
-        note.title = req.body.title;
-        note.content = req.body.content;
-
-        note.save(function(err, data){
-            if(err) {
-                res.status(500).send({message: "Could not update note with id " + req.params.noteId});
-            } else {
-                res.send(data);
+      try {
+        res.send(Location.aggregate(
+            [{
+          $geoNear: {
+           near: { type: "Point", coordinates: coords }, 
+           distanceField: "dist.calculated",
+           includeLocs: "dist.location",
+           spherical: true
             }
-        });
-    });
+            }]).limit(limit).exec());
+      } catch (error) {
+        res.status(500).send({message: "Some error occurred while retrieving notes.", error: error});
+      }
+
 };
 
-exports.delete = function(req, res) {
+exports.update = async function(req, res) {
+    // Update a note identified by the noteId in the request
+    try {
+        res(await Location.findByIdAndUpdate(req.params.noteId, {
+            title: req.body.title || "Untitled Note",
+            content: req.body.content
+        }, {new: true}).exec());
+    }
+    catch (error) {
+        res.status(500).send({message: "Some error occurred while retrieving notes.", error: error});
+    }
+
+};
+
+exports.delete = async function(req, res) {
     // Delete a note with the specified noteId in the request
-    Location.remove({_id: req.params.noteId}, function(err, data) {
-        if(err) {
-            res.status(500).send({message: "Could not delete note with id " + req.params.id});
-        } else {
-            res.send({message: "Note deleted successfully!"})
-        }
-    });
+    try {
+        res.send(await Location.findByIdAndRemove(req.params.noteId).exec());
+    }
+    catch(error) {
+        res.status(500).send({message: "Some error occurred while retrieving notes.", error: error});
+    }
 };
